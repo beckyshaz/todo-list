52681f92509c42e4474fc5dfd1bc5f027b96520c
beckyshaz <sharonsakah8@gmail.com>
2025-09-17 13:41:32 +0300

    creating todos structure , setting due dates using the date object, creating prioties of todos

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import { Todos } from "./project";
 
 import { createProjectForm } from "./project";
 
+import { createTodosForm } from "./project";
+
 const project = new Project("Love", "love is a beautiful thing when founded"); 
 console.log(project);
 
@@ -14,3 +16,5 @@ const todo = new Todos("washing", "washing clothes is diffult but we manage", 13
 console.log(todo);
 
 createProjectForm();
+
+createTodosForm();
--- src/project.js
+++ src/project.js
@@ -1,3 +1,6 @@
+
+import { format } from "date-fns";
+
 export class Project {
     constructor (title, description){
 
@@ -10,12 +13,16 @@ export function createProjectForm () {
     const projectForm = document.createElement("form");
     projectForm.className = "project-form";
     const titleInput = document.createElement("input");
-    titleInput.type = Text;
+    titleInput.type = "text";
     titleInput.value = "Project Name";
+    titleInput.id = "title"
+    titleInput.name = "project-name";
     titleInput.style.color = "rgba(0, 0, 0, 0.4)";
 
     const descriptionInput = document.createElement("input");
     descriptionInput.value = "Description";
+    descriptionInput.name = "project-description";
+    descriptionInput.id = "project-description";
     descriptionInput.style.color = "rgba(0, 0, 0, 0.4)";
     descriptionInput.style.border = "none";
 
@@ -55,3 +62,126 @@ export class Todos extends Project{
 }
 
 //const todo = new Todos("washing", "washing clothes is diffult but we manage", 13, "low", "done")
+
+
+export function  createTodosForm () {
+    const todosForm = document.createElement("form");
+    const titleTodoInput = document.createElement("input");
+    titleTodoInput.type = "text";
+    titleTodoInput.value = "Visit my Grandmother together with my family";
+    titleTodoInput.style.color = "rgba(0, 0, 0, 0.4)";
+
+    const descriptionTodoInput = document.createElement("input");
+    descriptionTodoInput.value = "Description";
+    descriptionTodoInput.style.color = "rgba(0, 0, 0, 0.4)";
+    descriptionTodoInput.style.border = "none";
+
+    const dateDiv = document.createElement("div");
+    dateDiv.className = "date-container";
+
+    const dateInput = document.createElement("input");
+    dateInput.type = "date";
+
+    const currentDate = format(new Date(), "yyyy-MM-dd");
+    console.log(currentDate);
+    //dateInput.value = currentDate;
+
+    dateInput.id = "mydate";
+    dateInput.name = "date";
+
+    //const currentDate = format(new Date(), "yyyy-MM-dd");
+    //console.log(currentDate);
+
+    const datepickerDiv = document.createElement("div");
+    datepickerDiv.append(dateInput);
+
+    const dateLabel = document.createElement("label");
+    dateLabel.className = "date-label";
+    dateLabel.setAttribute("for", "mydate");
+    dateLabel.innerHTML = "Set date";
+
+    const prioritiesDiv = document.createElement("div");
+    prioritiesDiv.textContent = "Priorities";
+    const priority1 = document.createElement("input");
+    priority1.type = "checkbox";
+    priority1.name = "priority1";
+    priority1.id = "priority1";
+
+    const priority1label = document.createElement("label");
+    priority1label.textContent = "very High";
+    priority1label.setAttribute("for", "priority1");
+
+    const priority1Div = document.createElement("div");
+    priority1Div.className = "priority1";
+
+    priority1Div.append(priority1, priority1label);
+
+
+    const priority2 = document.createElement("input");
+    priority2.type = "checkbox";
+    priority2.name = "priority2";
+    priority2.id = "priority2";
+
+    const priority2label = document.createElement("label");
+    priority2label.textContent = "High";
+    priority2label.setAttribute("for", "priority2");
+
+    const priority2Div = document.createElement("div");
+    priority2Div.className = "priority2";
+
+    priority2Div.append(priority2, priority2label);
+
+    const priority3 = document.createElement("input");
+    priority3.type = "checkbox";
+    priority3.name = "priority3";
+    priority3.id = "priority3";
+
+    const priority3label = document.createElement("label");
+    priority3label.textContent = "Medium";
+    priority3label.setAttribute("for", "priority3");
+
+    const priority3Div = document.createElement("div");
+    priority3Div.className = "priority3";
+
+    priority3Div.append(priority3, priority3label);
+
+    const priority4 = document.createElement("input");
+    priority4.type = "checkbox";
+    priority4.name = "priority4";
+    priority4.id = "priority4";
+
+    const priority4label = document.createElement("label");
+    priority4label.textContent = "Low";
+    priority4label.setAttribute("for", "priority4");
+
+    const priority4Div = document.createElement("div");
+    priority4Div.className = "priority4";
+
+    priority4Div.append(priority4, priority4label);
+
+    
+    prioritiesDiv.append(priority1Div,priority2Div, priority3Div, priority4Div );
+
+    const todoNotesDiv = document.createElement("div");
+    todoNotesDiv.className = "notes-div";
+
+    const notesLabel = document.createElement("label");
+    notesLabel.textContent = "Notes";
+
+    const todoNotes = document.createElement("textarea");
+    todoNotes.name = "notes";
+    todoNotes.rows = "10";
+    todoNotes.cols = "50";
+
+    todoNotesDiv.append(notesLabel, todoNotes);
+
+
+    dateDiv.append(titleTodoInput, descriptionTodoInput,dateLabel, datepickerDiv, prioritiesDiv, todoNotesDiv);
+    
+    todosForm.appendChild(dateDiv);
+    
+    const container = document.querySelector(".form-contents");
+
+    container.appendChild(todosForm);
+   
+}
